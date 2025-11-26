489600ec1e9e5b9910da0d3bb5471e4e53b49a0b
Marcus Friberg Isenborg <misenborg@hotmail.com>
2025-11-26 20:19:53 +0100

    create a demo scenario for testing kick

---
+++ software/controller/internal/ai/plan/test_kick.go
@@ -0,0 +1,57 @@
+package ai
+
+import (
+	"sync"
+	"time"
+
+	ai "github.com/LiU-SeeGoals/controller/internal/ai/activity"
+	"github.com/LiU-SeeGoals/controller/internal/info"
+)
+
+type TestKick struct {
+	plannerCore
+	at_state int
+	start    time.Time
+	max_time time.Duration
+}
+
+func NewTestKick(team info.Team) *TestKick {
+	return &TestKick{
+		plannerCore: plannerCore{
+			team: team,
+		},
+	}
+}
+
+func (m *TestKick) Init(
+	incoming <-chan info.GameInfo,
+	activities *[info.TEAM_SIZE]ai.Activity,
+	lock *sync.Mutex,
+	team info.Team,
+) {
+	m.incomingGameInfo = incoming
+	m.activities = activities // store pointer directly
+	m.activity_lock = lock
+	m.team = team
+	m.start = time.Now()
+
+	go m.run()
+}
+
+func wait(ms time.Duration) {
+	time.Sleep(ms)
+}
+
+func (g *TestKick) run() {
+	for {
+		wait(100 * time.Millisecond)
+		if g.activities[0] == nil {
+			queue := ai.NewActivityQueue(0, []ai.Activity{
+				ai.NewMoveToBall(g.team, 0),
+				ai.NewKickAtPosition(g.team, 0, info.Position{X: 2000, Y: 100}),
+			})
+			g.AddActivity(queue)
+		}
+	}
+
+}
