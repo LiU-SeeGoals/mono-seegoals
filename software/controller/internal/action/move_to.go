eae9f2163f75c92671b3afb2bfddc8a0a0c7c6d8
Marcus <46831591+Isenborg@users.noreply.github.com>
2025-12-08 20:01:50 +0100

    Working kick action (#29)
    * Fix possesor logic, update findPossesor to use dribbler position, and fixed error in getPosessor
    
    * Increase safety margin to better avoid ball (probably exists a better fix)
    
    * create a demo scenario for testing kick
    
    * attempt to fix angle issue at +- pi radians
    
    * semi working kick, still many edge cases that needs to be fixed
    
    * simple plan
    
    * main for running only kick test
    
    * Updated rotation/aiming logic, no longer rotates with the ball.
    
    * slightly changed hyperparameters for faster rotation
    
    * Add Dribble and Kickspeed to the Real action command
    
    * add minor comment
    
    * remove prints
    
    ---------
    
    Co-authored-by: jakob <45949712+jakob321@users.noreply.github.com>

--- software/controller/internal/action/move_to.go
+++ software/controller/internal/action/move_to.go
@@ -48,7 +48,7 @@ func (mv *MoveTo) simulateRealMovement() *simulation.RobotCommand {
 	// dy := mv.Pos.Y - mv.Dest.Y
 	dx := mv.Dest.X - mv.Pos.X
 	dy := mv.Dest.Y - mv.Pos.Y
-	angleDiff := mv.Dest.Angle - mv.Pos.Angle
+	angleDiff := info.NormalizeAngleDelta(mv.Dest.Angle, mv.Pos.Angle)
 
 	distance := math.Sqrt(dx*dx + dy*dy)
 	maxSpeed := float64(0.5)
@@ -165,12 +165,13 @@ func (mv *MoveTo) TranslateSim() *simulation.RobotCommand {
 }
 
 func (mt *MoveTo) TranslateReal() *robot_action.Command {
-	// var dribble int32
-	// if mt.Dribble {
-	// 	dribble = 1
-	// } else {
-	// 	dribble = 0
-	// }
+	// Robots only take binary commands for kick and dribblespeed.
+	// Either 0 or 1.
+	kickSpeedReal := min(1, mt.KickSpeed)
+	dribbleSpeedReal := 0
+	if mt.Dribble {
+		dribbleSpeedReal = 1
+	}
 
 	command_move := &robot_action.Command{
 		CommandId: robot_action.ActionType_MOVE_TO_ACTION,
@@ -185,6 +186,8 @@ func (mt *MoveTo) TranslateReal() *robot_action.Command {
 			Y: int32(mt.Dest.Y + 10000),
 			W: float32(mt.Dest.Angle * 1000),
 		},
+		KickSpeed:  int32(kickSpeedReal),
+		AngularVel: int32(dribbleSpeedReal),
 	}
 	return command_move
 }
