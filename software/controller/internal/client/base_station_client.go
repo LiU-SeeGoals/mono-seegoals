a898aa2047d23aa9a6c3062abf396626b97e37ac
Rasmus Wallin <...>
2025-12-10 17:03:36 +0100

    try on fet

--- software/controller/internal/client/base_station_client.go
+++ software/controller/internal/client/base_station_client.go
@@ -1,11 +1,10 @@
 package client
-
 import (
 	"errors"
 	"fmt"
 	"net"
 	"sync"
-
+	"time"
 	"github.com/LiU-SeeGoals/controller/internal/action"
 	"github.com/LiU-SeeGoals/proto_go/robot_action"
 	"google.golang.org/protobuf/proto"
@@ -22,31 +21,26 @@ type BaseStationClient struct {
 	connection    Connection
 	address       string
 	queueMutex    sync.Mutex
-	threadMutex   sync.Mutex
 	queue         []*robot_action.Command
 	hasBeenInited bool
 }
 
 func NewBaseStationClient(address string) *BaseStationClient {
-	var err error = nil
 	multicastIP := "239.0.0.2"
 	multicastPort := 9999
-
+	
 	addr, err := net.ResolveUDPAddr("udp", fmt.Sprintf("%s:%d", multicastIP, multicastPort))
 	if err != nil {
 		panic(err)
 	}
-
+	
 	connection, err := net.DialUDP("udp", nil, addr)
 	if err != nil {
 		panic(err)
 	}
-	//connection, _ := net.Dial("udp", address)
-	//if err != nil {
-	//	fmt.Printf("Some error %v\n", err)
-	//}
-
-	fmt.Println("Multicast server (basedstation) at ", multicastIP, multicastPort)
+	
+	fmt.Println("Multicast server (basestation) at", multicastIP, multicastPort)
+	
 	return &BaseStationClient{
 		connection:    connection,
 		address:       address,
@@ -58,42 +52,37 @@ func NewBaseStationClient(address string) *BaseStationClient {
 func (b *BaseStationClient) Init() {
 	go b.sendCommands()
 	b.hasBeenInited = true
-	b.threadMutex.Lock()
 }
 
-// Goroutine function for processing and sending commands
 func (b *BaseStationClient) sendCommands() {
 	for {
-
+		b.queueMutex.Lock()
 		if len(b.queue) == 0 {
-			// Wait to be unlocked
-			b.threadMutex.Lock()
+			b.queueMutex.Unlock()
+			time.Sleep(10 * time.Millisecond)
 			continue
 		}
-
-		// Process the first command in the queue
-		b.queueMutex.Lock()
+		
 		cmd := b.queue[0]
 		b.queue = b.queue[1:]
 		b.queueMutex.Unlock()
-
-		// Send the command
-		serializedCmd, _ := proto.Marshal(cmd) // Add error handling
-		b.sendMessage(serializedCmd)           // Add error handling
+		
+		serializedCmd, _ := proto.Marshal(cmd)
+		b.sendMessage(serializedCmd)
 	}
 }
 
-// send list of actions to the base station
 func (b *BaseStationClient) SendActions(actions []action.Action) {
 	if !b.hasBeenInited {
 		fmt.Println("\033[0m Base station client has not been inited\033[33m")
+		return
 	}
+	
+	b.queueMutex.Lock()
 	for _, action := range actions {
-		b.queueMutex.Lock()
 		b.queue = append(b.queue, action.TranslateReal())
-		b.queueMutex.Unlock()
 	}
-	b.threadMutex.Unlock()
+	b.queueMutex.Unlock()
 }
 
 func (b *BaseStationClient) sendMessage(input []byte) error {
@@ -101,7 +90,6 @@ func (b *BaseStationClient) sendMessage(input []byte) error {
 		fmt.Print("to big to send (if sent = Rasmus mad 😡)")
 		return errors.New("too long message")
 	}
-
 	_, err := b.connection.Write(input)
 	if err != nil {
 		fmt.Printf("Some error %v\n", err)
