3e4adfb0fdd57f8274e0147bedfc97744c10a19b
Rasmus Wallin <...>
2026-01-20 15:39:18 +0100

    tracked maybe

--- software/controller/internal/info/game_state.go
+++ software/controller/internal/info/game_state.go
@@ -9,6 +9,18 @@ import (
 
 const TEAM_SIZE ID = 16
 
+type KickedBallInfo struct {
+	StartX    float64
+	StartY    float64
+	StartTS   float64
+	StopX     float64
+	StopY     float64
+	StopTS    float64
+	RobotTeam Team
+	RobotId   uint32
+	Valid     bool
+}
+
 type GameState struct {
 	Valid       bool
 	Blue_team   *RobotTeam
@@ -16,21 +28,32 @@ type GameState struct {
 
 	Ball *Ball
 
+	TrackedBlue   [TEAM_SIZE]*TrackedRobot
+	TrackedYellow [TEAM_SIZE]*TrackedRobot
+	TrackedBall   *TrackedBall
+
+	KickedBall KickedBallInfo
+
 	MessageReceived int64
+	Timestamp       float64
 }
 
-// Constructor for game state
 func NewGameState(capacity int) *GameState {
 	gs := GameState{}
 	gs.Valid = true
 
 	gs.Ball = NewBall(capacity)
+	gs.TrackedBall = NewTrackedBall()
+
 	var i ID
 	gs.Blue_team = new(RobotTeam)
 	gs.Yellow_team = new(RobotTeam)
 	for i = 0; i < TEAM_SIZE; i++ {
 		gs.Blue_team[i] = NewRobot(i, Blue, capacity)
 		gs.Yellow_team[i] = NewRobot(i, Yellow, capacity)
+
+		gs.TrackedBlue[i] = NewTrackedRobot(uint32(i), Blue)
+		gs.TrackedYellow[i] = NewTrackedRobot(uint32(i), Yellow)
 	}
 	return &gs
 }
@@ -67,79 +90,59 @@ func (gs *GameState) ClosestRobot(target Position) (*Robot, float64) {
 	return closestRobot, shortestDistance
 }
 
-// Should be called directly after inserting
-// new data, recieved from ssl vision, into
-// the game state
 func (gs *GameState) Update() {
-
-	// Update robot estimates
-	// Has to be done before updating ball possessor
 	for _, robot := range gs.Blue_team {
 		if !robot.IsActive() {
 			continue
 		}
-		// robot.Update()
 	}
 	for _, robot := range gs.Yellow_team {
 		if !robot.IsActive() {
 			continue
 		}
-		// robot.Update()
 	}
 
 	latestBallPos, _ := gs.Ball.GetPosition()
 	latestPossessor := gs.Ball.GetPossessor()
 	newPossessor := gs.FindBallPossessor()
 
-	if gs.Ball.GetAge() < 50 { // Have a new ball measurement,  WARN: Magic number
-		// fmt.Println("New ball measurement")
+	if gs.Ball.GetAge() < 50 {
 		gs.Ball.SetEstimatedPosition(latestBallPos)
 
 		if latestPossessor != nil {
-			if gs.LostBall(latestPossessor) { // We have a new measurement, so we can check if possessor has lost the ball
+			if gs.LostBall(latestPossessor) {
 				gs.Ball.SetPossessor(nil)
 			}
 		}
 
-		// If we have a new possessor (and either didn't have one before, or just lost it), update it.
-		// Note: This allows instant "stealing" logic if we wanted, but primarily handles the case where latestPossessor was nil.
 		if gs.Ball.GetPossessor() == nil && newPossessor != nil {
 			gs.Ball.SetPossessor(newPossessor)
 		}
 
-	} else if latestPossessor != nil { // No new ball measurement, so we have to rely on the latest possessor
-
-		// fmt.Println("No new ball measurement but we have a possessor")
+	} else if latestPossessor != nil {
 		gs.Ball.SetEstimatedPosition(latestPossessor.DribblerPos())
-	} else if newPossessor != nil { // A robot has arrived at the latest location of the ball
-		// fmt.Println("No new ball measurement but we have a new possessor")
+	} else if newPossessor != nil {
 		gs.Ball.SetPossessor(newPossessor)
 		gs.Ball.SetEstimatedPosition(newPossessor.DribblerPos())
 
-	} else { // No new ball measurement and no possessor
-
+	} else {
 		gs.Ball.SetEstimatedPosition(latestBallPos)
 	}
-
 }
 
 func (gs *GameState) FindBallPossessor() *Robot {
-
 	ballPos, err := gs.Ball.GetPosition()
 	if err != nil {
 		Logger.Errorf("Ball position retrieval failed: %v", err)
 		return nil
 	}
 
-	// Get the robot closest to the ball
 	closestToBall, _ := gs.ClosestRobot(ballPos)
 
-	// Threshold for possession.
 	const possessionThreshold = 30.0
 
 	if closestToBall != nil {
 		dist := ballPos.Distance(closestToBall.DribblerPos())
-		// fmt.Printf("[DEBUG] Robot %d dist to ball dribbler: %.2f (threshold: %.2f)\n", closestToBall.ID(), dist, possessionThreshold)
 		if dist < possessionThreshold {
 			return closestToBall
 		}
@@ -159,12 +162,9 @@ func (gs *GameState) LostBall(robot *Robot) bool {
 		return true
 	}
 
-	farAway := ballPos.Distance(robot.DribblerPos()) > 50 // WARN: Magic number, mm
+	farAway := ballPos.Distance(robot.DribblerPos()) > 50
 
-	// If the robot is not facing the ball
-	// it has lost the ball
-	return !robot.Facing(ballPos, 0.5) || farAway // WARN: Magic number
-
+	return !robot.Facing(ballPos, 0.5) || farAway
 }
 
 func (gs *GameState) SetValid(valid bool) {
@@ -183,18 +183,66 @@ func (gs *GameState) SetBlueRobot(robotId uint32, x, y, angle float64, time int6
 	gs.Blue_team[robotId].SetPositionTime(x, y, angle, time)
 }
 
-// Updates position of robots and balls to their actual position
 func (gs *GameState) SetBall(x, y, z float64, time int64) {
 	gs.Ball.SetPositionTime(x, y, z, time)
 }
 
 func (gs *GameState) SetMessageReceivedTime(time int64) {
 	gs.MessageReceived = time
-
 }
 
 func (gs *GameState) GetMessageReceivedTime() int64 {
 	return gs.MessageReceived
+}
+
+func (gs *GameState) SetTimestamp(ts float64) {
+	gs.Timestamp = ts
+}
+
+func (gs *GameState) SetTrackedRobot(team Team, id uint32, pos Position, vel Position, angle float64, velAngular float64, ts float64) {
+	switch team {
+	case Yellow:
+		gs.TrackedYellow[id].SetTracked(pos, vel, angle, velAngular, ts)
+	case Blue:
+		gs.TrackedBlue[id].SetTracked(pos, vel, angle, velAngular, ts)
+	}
+}
+
+func (gs *GameState) SetTrackedBall(pos Position, vel Position, ts float64) {
+	gs.TrackedBall.SetTracked(pos, vel, ts)
+}
+
+func (gs *GameState) GetTrackedRobot(team Team, id uint32) *TrackedRobot {
+	switch team {
+	case Yellow:
+		return gs.TrackedYellow[id]
+	case Blue:
+		return gs.TrackedBlue[id]
+	default:
+		return nil
+	}
+}
+
+func (gs *GameState) GetTrackedBall() *TrackedBall {
+	return gs.TrackedBall
+}
+
+func (gs *GameState) SetKickedBall(startX, startY, startTS, stopX, stopY, stopTS float64, team Team, id uint32) {
+	gs.KickedBall = KickedBallInfo{
+		StartX:    startX,
+		StartY:    startY,
+		StartTS:   startTS,
+		StopX:     stopX,
+		StopY:     stopY,
+		StopTS:    stopTS,
+		RobotTeam: team,
+		RobotId:   id,
+		Valid:     true,
+	}
+}
+
+func (gs *GameState) ClearKickedBall() {
+	gs.KickedBall.Valid = false
 }
 
 func (gs *GameState) GetBall() *Ball {
@@ -229,7 +277,6 @@ func (gs *GameState) GetRobot(id ID, team Team) *Robot {
 	return gs.GetTeam(team)[id]
 }
 
-// String representation of game state
 func (gs *GameState) String() string {
 	gs_str := "{\n blue team: {\n"
 	var i ID
@@ -240,12 +287,6 @@ func (gs *GameState) String() string {
 	for i = 0; i < TEAM_SIZE; i++ {
 		gs_str += "robot: {" + gs.Yellow_team[i].String() + " },\n"
 	}
-	// for _, line := range gs.Field.FieldLines {
-	// 	gs_str += fmt.Sprintf("line: {%s}\n", line.String())
-	// }
-	// for _, arc := range gs.Field.FieldArcs {
-	// 	gs_str += fmt.Sprintf("arc: {%s}\n", arc.String())
-	// }
 	gs_str += "}"
 	return gs_str
 }
@@ -271,8 +312,8 @@ func (gs GameState) ToDTO() *GameStateDTO {
 func (gs GameState) ToJson() []byte {
 	gameStateJson, err := json.Marshal(gs.ToDTO())
 	if err != nil {
-		// fmt.Println("The gamestate packet could not be marshalled to JSON.")
 		Logger.Error("The gamestate packet could not be marshalled to JSON.")
 	}
 	return gameStateJson
 }
+
