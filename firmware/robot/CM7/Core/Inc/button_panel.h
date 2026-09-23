ae8cae28d0e6c245a4e625e16cf7ab9b31b99b6f
Dogerzs <aron.molander@telia.com>
2026-09-23 19:05:50 +0200

    Basic partitioning of code

---
+++ firmware/robot/CM7/Core/Inc/button_panel.h
@@ -0,0 +1,14 @@
+#ifndef BUTTON_PANEL_H
+#define BUTTON_PANEL_H
+
+// INCLUDES
+#include "main.h"
+
+/* Public variables */
+// ...
+
+/* Public function */
+
+
+
+#endif /* BUTTON_PANEL_H */
---
+++ firmware/robot/CM7/Core/Src/button_panel.c
@@ -0,0 +1,21 @@
+#include "button_panel.h"
+
+
+/* Private includes */
+
+
+/* Private defines */
+
+
+/* Private enums/structs */
+
+
+/* Private variables */
+
+
+/* Private functions declarations */
+
+
+/* Public functions implementations */
+
+/* Private functions implementations */
